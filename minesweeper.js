fe01b060d7d1046164a3d3dc55c1e583267285c9
Boyd_Thurston <boydthurston@gmail.com>
2020-03-02 15:30:34 +1300

    Added functionality
    added game reset, difficulty levels, choice of board sizes and
    random seeding of mines.

--- minesweeper.js
+++ minesweeper.js
@@ -1,77 +1,89 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-  cells: [
-    {
-      row: 0,
-      col: 0,
-      isMine: true,
-      hidden: true
-    },
-      {
-        row: 0,
-        col: 1,
-        isMine: false,
-        hidden: true
-      },
-        {
-        row: 0,
-        col: 2,
-        isMine: false,
-        hidden: true
-        },
-    {
-      row: 1,
-      col: 0,
-      isMine: false,
-      hidden: true
-    },
-      {
-        row: 1,
-        col: 1,
-        isMine: true,
-        hidden: true
-      },
-        {
-          row: 1,
-          col: 2,
-          isMine: true,
-          hidden: true
-        },
-    {
-      row: 2,
-      col: 0,
-      isMine: false,
-      hidden: true
-    },
-      {
-        row: 2,
-        col: 1,
-        isMine: false,
-        hidden: true
-      },
-        {
-          row: 2,
-          col: 2,
-          isMine: false,
-          hidden: true
-        }
-  ]
-}
+var board = {}
+
 
 function startGame () {
+  // calls function to generate the game board
+  generateBoard();
+  
+  // loops through cells array to get number of surrounding mines for each cell
   for(let i = 0; i < board.cells.length; i++) {
-  //  board.cells[i].surroundingMines = 0;
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   };
+
+  // calls function to check for win conditions
   document.addEventListener("click",checkForWin);
   document.addEventListener("contextmenu",checkForWin);
+
+  // gets board size
+  let boardSizeRadios = document.getElementsByName('boardSize')
+  for (let i = 0; i < boardSizeRadios.length; i++) {
+    boardSizeRadios[i].addEventListener("click", resetBoard)
+  }
+
+  // gets difficulty
+  let difficultyRadios = document.getElementsByName('difficulty')
+  for (let i = 0; i < difficultyRadios.length; i++) {
+    difficultyRadios[i].addEventListener("click", resetBoard)
+  }
+
+  // resets game board
+  document.getElementById('reset').addEventListener("click",resetBoard);
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
 
-// Define this function to look for a win condition:
+// generates the game board
+function generateBoard() {
+  // creating variables
+  board.cells = []
+  boardSize = Number(document.querySelector('input[name="boardSize"]:checked').value)
+  difficulty = getDifficulty()
+
+  // creating cells
+  for (i=0; i<boardSize; i++) {
+    for(j=0; j<boardSize; j++) {
+      board.cells.push({
+        row: i,
+        col: j,
+        isMine: Math.random() < difficulty, 
+        hidden: true
+      })
+    }
+  }
+}
+
+// Reset the board function
+function resetBoard() {
+  // Remove existing board
+  document.getElementsByClassName('board')[0].innerHTML = '';
+
+  // Run the initial game setup again
+  startGame()
+}
+
+// sets game difficulty
+function getDifficulty() {
+  // checks radio button selection
+  radioSelect = document.querySelector('input[name="difficulty"]:checked').value
+
+  // sets random seed based on radioSelect
+  if(radioSelect == "hard"){
+    return 0.66;
+  } else if (radioSelect == "normal"){
+    return 0.55;
+  } else if (radioSelect == "easy"){
+    return 0.33;
+  } else {
+    // if issue return default value
+    return 0.55
+  }
+}
+
+// function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
@@ -99,19 +111,10 @@ function checkForWin () {
   // checking for win
   if( isMarkedCount == isMineCount && isHiddenCount == 0){
     lib.displayMessage('You win!')
-  }
-
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   
+  } 
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+// function to count the number of mines around the cell
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
@@ -124,7 +127,7 @@ function countSurroundingMines (cell) {
       count++;
     }
   };
- console.log('count for cell ' + cell.row + ',' + cell.col + ' is: ' + count);
+//  console.log('count for cell ' + cell.row + ',' + cell.col + ' is: ' + count);
   return count;
 }
 
